c98f0249c83ae8f255fff48a12182c7c1d24149f
rzondervan <rzondervan@gmail.com>
2022-01-28 19:20:23 +0100

    tweeked comment

--- gitsetup.sh
+++ gitsetup.sh
@@ -1,6 +1,24 @@
+# script to setup the git versioning
+# nooit gebruikt, wel copy per line naar cmdline
 
+/bin/sh gitsetup.sh
 git config --global user.name "rzondervan"
 git config --global user.email "rzondervan@gmail.com"
 git config --global push.default matching
 git config --global alias.co checkout
 git init
+
+#De url moest in de configfile worden aangepast (van https naar ssh):
+#(virt) rzondervan@penguin:~/flasker$ cat .git/config 
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "origin"]
+        url = ssh://git@github.com/rzondervan/flasker.git
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "main"]
+        remote = origin
+        merge = refs/heads/main
+git config --global init.defaultBranch main
--- hello.py
+++ hello.py
@@ -43,7 +43,7 @@ def user(name):
 def page_not_found(e):
 	return render_template("404.html"), 404
 
-# Internal Server Error
+# Internal Server Error thing
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
