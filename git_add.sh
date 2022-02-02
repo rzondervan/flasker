3b782cd5790db7a6def203356bc45ad5740d8c55
rzondervan <rzondervan@gmail.com>
2022-02-02 21:35:14 +0100

    flask friday #6 gedaan - wtforms en messages

--- git_add.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git add .
-git commit -m 'flask friday #4 gedaan'
-git push -u origin main
---
+++ git_add_eerst_commentinhoud_aanpassen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git add .
+git commit -m 'flask friday #6 gedaan - wtforms en messages'
+git push -u origin main
